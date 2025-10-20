266ca5ba4bde69d4350dfc67fa8de51b9489aa62
Lucas Duarte <lucasduartedev@gmail.com>
2025-10-20 16:14:26 -0300

    Início do projeto

---
+++ README.md
@@ -0,0 +1,12 @@
+# API Rest em Java com Spring Boot
+
+## Objetivo
+
+<p>Desenvolver uma API Rest simulando o funcionamento de um sistema interno de um mercado.</p>
+
+## Tecnoogias
+<ul>
+    <li>Linguagem: Java 17</li>
+    <li>Banco de dados: MySQL</li>
+    <li>Spring Boot</li>
+</ul>
---
+++ src/main/java/com/api/mercado/ApimercadoApplication.java
@@ -0,0 +1,13 @@
+package com.api.mercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimercadoApplication.class, args);
+	}
+
+}
